2aa5d72408cfb48f60140b9377db510a79e1d5d5
KARTIK R MATHUR <38400944+kartikrmathur@users.noreply.github.com>
2021-07-01 17:14:05 +0530

    Add files via upload

---
+++ FinalBillViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FinalBillViewController.swift
+//  new
+//
+//  Created by Mathur on 20/07/18.
+//  Copyright © 2018 Apple inc. All rights reserved.
+//
+
+import UIKit
+
+class FinalBillViewController: UIViewController {
+    var arrSelectedData =  NSMutableArray();
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
